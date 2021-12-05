b33919188ba9325b67c7306b629470c784f9dc81
pringgojs <odyinggo@gmail.com>
2021-12-05 21:22:26 +0700

    creating controller method

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,42 +1,15 @@
 const express = require('express')
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp
+} = require('../controllers/bootcamps')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: 'show all bootcamps'
-    })
-})
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `show bootcamp id ${req.params.id}`
-    })
-})
-
-
-router.post('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `create new bootcamp`
-    })
-})
-
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `update bootcamp id ${req.params.id}`
-    })
-})
-
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `delete bootcamp id ${req.params.id}`
-    })
-})
+router.route('/').get(getBootcamps).post(createBootcamp)
+router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
 module.exports = router
