f8e3f0e7948da94cea5519855d1a6803d22a5546
pringgojs <odyinggo@gmail.com>
2021-12-07 02:19:52 +0700

    setup mongoose with cloud database

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({
     path: './config/config.env'
 });
 
+// Connect to database
+connectDB();
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
 
-// Loggin in middleware
+// Logging in middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -21,7 +24,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening in ${process.env.NODE_ENV} mode n port ${PORT}!`);
 });
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
